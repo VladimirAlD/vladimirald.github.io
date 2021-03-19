c1aa6548e6f42b570da4df1af2a2352260fea8d1
VladimirAlD <gglmail96@gmail.com>
2021-03-19 17:24:34 +0500

    Added Tiny-Slider

--- projects/i-am-woman/js/tinySlider__script.js
+++ projects/i-am-woman/js/tinySlider__script.js
@@ -1,42 +1,58 @@
-// const sliderMerch = tns({
-//   container: '.my-slider__slides-container',
-//   controlsContainer: '.my-slider__controls',
-//   rewind: true,
-//   responsive: {
-//     600: {
-//       items: 2,
-//       gutter: 20
-//     },
-//     768: {
-//       items: 3,
-//       controls: false
-//     }
-//   },        
-//   slideBy: 'page',
-//   mouseDrag: true,
-//   swipeAngle: 10,
-//   nav: false,
-//   speed: 500,
-//   preventScrollOnTouch: 'auto'
-// });
-
-// const sliderBlog = tns({
-//   container: '.blog-content',
-//   controls: false,
-//   rewind: true,
-//   responsive: {
-//     600: {
-//       items: 2
-//     },
-//     1180: {
-//       items: 3,
-//     }
-//   },
-//   gutter: 20,
-//   slideBy: 'page',
-//   mouseDrag: true,
-//   swipeAngle: 10,
-//   nav: false,
-//   speed: 500,
-//   preventScrollOnTouch: 'auto'
-// });
+const sliderIntro = tns({
+  container: '.js__sliderIntro',
+  items: 1,
+  rewind: true,
+  autoplay: true,
+  autoplayButtonOutput: false,
+  controls: true,
+  prevButton: '.slider-intro__nav .js__prev',
+  nextButton: '.slider-intro__nav .js__next',
+  nav: false,
+  mouseDrag: true,
+  speed: 500,
+  swipeAngle: 30,
+  preventScrollOnTouch: 'auto'
+})
+const sliderMerch = tns({
+  container: '.js__sliderMerch',
+  items: 1,
+  loop: false,
+  autoplayButtonOutput: false,
+  controls: true,
+  prevButton: '.slider-merch__nav .js__prev',
+  nextButton: '.slider-merch__nav .js__next',
+  nav: false,
+  mouseDrag: true,
+  swipeAngle: 30,
+  preventScrollOnTouch: 'auto',
+  responsive: {
+    764: {
+      gutter: 20,
+      items: 3
+    },
+    1180: {
+      gutter: 30
+    }
+  },
+})
+const sliderBlog = tns({
+  container: '.js__sliderBlog',
+  items: 1,
+  loop: false,
+  autoplayButtonOutput: false,
+  controls: false,
+  nav: false,
+  mouseDrag: true,
+  swipeAngle: 30,
+  preventScrollOnTouch: 'auto',
+  responsive: {
+    764: {
+      gutter: 20,
+      items: 2
+    },
+    1180: {
+      items: 3,
+      gutter: 30,
+    }
+  }
+})
